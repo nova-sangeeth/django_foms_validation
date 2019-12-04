0c282f5c4319e8a990f903f8ec2906567f56edf5
nova-sangeeth <novasangeeth@outlook.com>
2019-12-04 16:05:56 +0530

    added conditions for sorting by date and count. added a method to do validate the parameters which are entered in the input box (if none then go on)

--- src/core/views.py
+++ src/core/views.py
@@ -1,27 +1,44 @@
 from django.shortcuts import render
 from .models import Jornal
-from django .db.models import  Q
+from django.db.models import Q
 
 
 def is_valid_queryparam(param):
     return param != '' and param is not None
+
+
 # Create your views here.
 def BootstrapFilterView(request):
     qs = Jornal.objects.all()
     title_contains_query = request.GET.get('title_contains')
     id_exact_query = request.GET.get('id_exact')
     title_or_author_query = request.GET.get('title_or_author')
+    view_count_min = request.GET.get('view_count_min')
+    view_count_max = request.GET.get('view_count_max')
+    date_min = request.GET.get('date_min')
+    date_max = request.GET.get('date_max')
 
-    if title_contains_query !='' and title_contains_query is not None:
-        qs = qs.filter(title__icontains= title_contains_query)
+    if is_valid_queryparam(title_contains_query):
+        qs = qs.filter(title__icontains=title_contains_query)
 
-    elif id_exact_query != '' and id_exact_query is not None:
+    elif is_valid_queryparam(id_exact_query):
         qs = qs.filter(title__icontains=id_exact_query)
 
-    elif title_or_author_query != '' and title_or_author_query is not None:
-        qs = qs.filter(Q(title__icontains=title_or_author_query) | Q(author__name__icontains=title_or_author_query) ).distinct()
+    elif is_valid_queryparam(title_or_author_query):
+        qs = qs.filter(
+            Q(title__icontains=title_or_author_query) | Q(author__name__icontains=title_or_author_query)).distinct()
+
+    if is_valid_queryparam(view_count_min):
+        qs = qs.filter(views__gte=view_count_min)
+    if is_valid_queryparam(view_count_max):
+        qs = qs.filter(views__lt=view_count_max)
+
+    if is_valid_queryparam(date_min):
+        qs = qs.filter(publish_date__gte=date_min)
+    if is_valid_queryparam(date_max):
+        qs = qs.filter(publish_date__lt=date_max)
 
     context = {
-        'queryset' : qs
+        'queryset': qs
     }
     return render(request, "bootstrap_form.html", context)
